5b9acdf39947be80807fd943d230160fac6b5ccd
Alfred Githinji <alfredgithinji87@gmail.com>
2022-05-03 13:27:32 +0300

    .

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 require("dotenv/config");
 const app = require("./index");
+const port = process.env.PORT || 3000;
 
 //CONNECTING THE SERVER TO MONGOOSE.
 //===================================
-mongoose.connect(process.env.REACT_APP_BACKEND_URL, {
+const connectionUrl =
+  "mongodb+srv://FredzTech:Beijingbike5@cluster0.a9nya.mongodb.net/freds_portfolio?retryWrites=true&w=majority";
+const otherConnectionUrl = process.env.REACT_APP_BACKEND_URL;
+mongoose.connect(connectionUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 // CONNECTION TEST
 //=================
 const db = mongoose.connection;
@@ -15,7 +20,6 @@ db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("The freds portfolio database connected successfully.");
 });
-const port = process.env.PORT || 3000;
 
 // CONFIGURING THE LISTENING PORT.
 //================================
