ad97c351cd902abce9d1130f2c16971353f809e0
Alfred Githinji <alfredgithinji87@gmail.com>
2022-05-03 13:31:46 +0300

    .

--- controllers/homePageControllers.js
+++ controllers/homePageControllers.js
@@ -1,6 +1,15 @@
-const testController = (req, res) => {
+const rootController = (req, res) => {
   try {
     res.send("Hello there from homepage route handler in action.").status(200);
+  } catch (error) {
+    res.send(error).status(500);
+  }
+};
+const testController = (req, res) => {
+  try {
+    res
+      .send("Hello there from homepage test route handler in action.")
+      .status(200);
   } catch (error) {
     res.send(error).status(500);
   }
@@ -16,4 +25,4 @@ const connectionController = (req, res) => {
   }
 };
 
-module.exports = { testController, connectionController };
+module.exports = { rootController, testController, connectionController };
--- routes/homePageRoute.js
+++ routes/homePageRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const {
+  rootController,
   testController,
   connectionController,
 } = require("../controllers/homePageControllers");
 
+// ROUTING IN ACTION
+//===================
+router.get("/", rootController);
 router.get("/test", testController);
 router.get("/connection", connectionController);
 
